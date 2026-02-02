59cb565dd5efe6d14e5d40f2baabbb3520c0a29e
dcayll <60239356+dcayll@users.noreply.github.com>
2026-02-01 22:37:07 -0600

    remove duplicate

--- _plugins/image_path_fixer.rb
+++ _plugins/image_path_fixer.rb
@@ -1,18 +1,24 @@
 module Jekyll
-  Jekyll::Hooks.register :site, :post_read do |site|
-    site.posts.docs.each { |doc| fix_asset_paths(doc) }
-    site.collections.each do |_name, collection|
-      collection.docs.each { |doc| fix_asset_paths(doc) }
+  class FixAssetPaths
+    def self.process(site)
+      site.posts.docs.each { |doc| fix_paths(doc) }
+      site.collections.each do |_name, collection|
+        collection.docs.each { |doc| fix_paths(doc) }
+      end
+      site.pages.each { |page| fix_paths(page) }
     end
-    site.pages.each { |page| fix_asset_paths(page) }
-  end
 
-  def self.fix_asset_paths(document)
-    return unless document.respond_to?(:content)
-    
-    # Replace (assets/ with (/assets/
-    document.content.gsub!(/\(assets\//, '(/assets/')
-    # Replace src="assets/ with src="/assets/
-    document.content.gsub!(/src="assets\//, 'src="/assets/')
+    def self.fix_paths(document)
+      return unless document.respond_to?(:content) && document.content
+      
+      # Replace (assets/ with (/assets/
+      document.content.gsub!(/\(assets\//, '(/assets/')
+      # Replace src="assets/ with src="/assets/
+      document.content.gsub!(/src="assets\//, 'src="/assets/')
+    end
   end
+end
+
+Jekyll::Hooks.register :site, :post_read do |site|
+  Jekyll::FixAssetPaths.process(site)
 end
