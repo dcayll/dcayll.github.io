8613b91c80a02add437488abea3f1ded6c0f1bd2
dcayll <60239356+dcayll@users.noreply.github.com>
2026-02-01 22:16:19 -0600

    updtae jekyll stuff

--- _plugins/asset_filter.rb
+++ _plugins/asset_filter.rb
@@ -12,6 +12,9 @@ module Jekyll
       site.collections.each do |name, collection|
         collection.docs.each { |doc| fix_image_paths(doc) }
       end
+      
+      # Also process pages
+      site.pages.each { |page| fix_image_paths(page) }
     end
 
     private
@@ -20,10 +23,14 @@ module Jekyll
       return unless document.content
       
       # Fix markdown image syntax: ![](assets/ → ![](/assets/
-      document.content.gsub!(/!\[\]\(assets\//, '![](/assets/')
+      if document.content.include?('![](assets/')
+        document.content.gsub!(/!\[\]\(assets\//, '![](/assets/')
+      end
       
       # Fix HTML img tags: src="assets/ → src="/assets/
-      document.content.gsub!(/src="assets\//, 'src="/assets/')
+      if document.content.include?('src="assets/')
+        document.content.gsub!(/src="assets\//, 'src="/assets/')
+      end
     end
   end
 end
